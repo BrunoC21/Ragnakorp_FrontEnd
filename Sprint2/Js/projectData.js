b82d14cc0e09c37336454f951f268f61611659cf
diego arteaga mendoza <daarteagamendoza@gmail.com>
2024-10-30 00:09:43 -0300

    Js userData y projectData

--- Sprint2/Js/projectData.js
+++ Sprint2/Js/projectData.js
@@ -1,9 +1,9 @@
-window.onload = function(){
+window.onload = function() {
     listarProyectos();
 }
 
 
-let listarProyectos = async ()=> {
+let listarProyectos = async ()=>{
     const peticion = await fetch("http://localhost:8080/proyecto/project/search",
         {
             method: "GET",
@@ -11,18 +11,20 @@ let listarProyectos = async ()=> {
                 "Accept": "application/json",
                 "Content-Type": "application/json"
             }
+        });
+
+        const proyectos = await peticion.json();
+
+        let contenidoTabla = "";
+        for (let proyecto of proyectos) {
+            contenidoTabla += `
+                <tr>
+                    <td>${proyecto.projName}</td>
+                    <td>${proyecto.projDescription}</td>
+                    <td>${proyecto.projRequirementsPostulation}</td>
+                </tr>
+            `;
         }
-    );
 
-    const proyectos = await peticion.json();
-
-    let contenidoProyectos = "";
-
-    for (let project of proyectos) {
-        let contenidoFila = '<tr> <td>${project.projName}</td><td>${project.projDescription}</td><td>${project.projStartDate}</td><td>${project.projRequirementsPostulation}</td> </tr>'
-
-        contenidoProyectos += contenidoFila;
-    }
-
-    document.querySelector("#tablaProyectos tbody").outerHTML = contenidoProyectos;
+        document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
 }
