20e74fde25d702a39dc1e7982bca5d4a252842fa
diego arteaga mendoza <daarteagamendoza@gmail.com>
2024-11-27 19:29:43 -0300

    Conexion para mostrar postulaciones

--- Sprint2/Js/showPostulations.js
+++ Sprint2/Js/showPostulations.js
@@ -1,3 +1,58 @@
+window.onload = function () {
+  obtenerPostulaciones();
+};
+
+let postulaciones = [];
+
+let obtenerPostulaciones = async () => {
+  const peticion = await fetch("http://localhost:8080/proyecto/postulation/search", {
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+  });
+
+  postulaciones = await peticion.json();
+  renderizarTabla("todos");
+}
+
+let renderizarTabla = (filtroCategoria) => {
+  let contenidoTabla = "";
+
+  const postulacionesFiltradas = filtroCategoria === "todos"
+    ? postulaciones
+    : postulaciones.filter(postulaciones => postulaciones.postulationStatus.toLowerCase() === filtroCategoria.toLowerCase());
+
+  for (let Postulacion of postulacionesFiltradas) {
+    contenidoTabla += `
+        <tr>
+            <td>${Postulacion.postulationRut}</td>
+            <td>${Postulacion.postulationProject}</td>
+            <td class="status status-pending">${Postulacion.postulationStatus}</td>
+            <td><button onclick="toggleDetails(this)">+</button></td>
+        </tr>
+        <tr class="hidden">
+            <td colspan="4">
+                <p><strong>RUT:</strong> ${Postulacion.postulationRut}</p>
+                <p><strong>Nombre:</strong> ${Postulacion.postulationName}</p>
+                <p><strong>Descripción:</strong> ${Postulacion.postulationDescription}</p>
+                <button onclick="acceptPostulation(this)">Aceptar</button>
+                <button onclick="rejectPostulation(this)">Rechazar</button>
+                <div class="hidden rejection-reason">
+                    <p>Justifique el rechazo:</p>
+                    <textarea rows="3"></textarea>
+                    <button onclick="submitRejection(this)">Enviar Justificación</button>
+                </div>
+            </td>
+        </tr>
+    `;
+  }
+
+  document.querySelector("#postulaciones tbody").innerHTML = contenidoTabla;
+
+}
+
 function toggleDetails(button) {
   const detailRow = button.parentElement.parentElement.nextElementSibling;
   detailRow.classList.toggle("hidden");
