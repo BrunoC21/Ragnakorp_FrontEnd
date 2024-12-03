8d3f5e28c6ec5c130c1e01c0a22be6f8ddd3e8a4
diego arteaga mendoza <daarteagamendoza@gmail.com>
2024-12-03 00:16:05 -0300

    ver cambios de noticias POR TERMINAR

---
+++ Sprint2/Js/showCambiosNoticias.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    obtenerCambios();
+};
+
+let cambios = [];
+
+let obtenerCambios = async () => {
+    const peticion = await fetch('http://localhost:8080/proyecto/chagnes/search/news', {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    });
+
+    cambios = await peticion.json();
+    renderizarTablaCambios();
+}
+
+let renderizarTablaCambios = () => {
+    let contenidoTabla = "";
+    
+    for (let cambio in cambios) {
+        contenidoTabla +=`
+            <tr>
+                <td>Actualizacion</td>
+                <td>${cambio.changeIdUser}</td>
+                <td>${cambio.changesDate}</td>
+                <td>${cambio.changesThing}</td>
+            </tr>
+        `;
+    }
+    document.querySelector("#tabla tbody").innerHTML = contenidoTabla;
+};
